e791721dcbd7d76fd85f2882d28324de7d509e27
Micefy <miccheli@gmail.com>
2015-05-06 13:47:23 +0300

    Some last scripts from vid#1

---
+++ Assets/Scripts/DamageOnTriggerEnter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageOnTriggerEnter : MonoBehaviour
+{
+	private DamageInfo damageInfo;
+
+	// Use this for initialization
+	void Start ()
+	{
+		damageInfo = GetComponent<DamageInfo>();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter(Collider col)
+	{
+		col.BroadcastMessage("Damage",damageInfo.RollDamage(),SendMessageOptions.DontRequireReceiver);
+	}
+}
