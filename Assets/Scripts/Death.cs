cae83b340d4760b432d630b9e3a279e07f4d87b3
Micefy <miccheli@gmail.com>
2015-05-06 16:21:55 +0300

    Added scripts from 2nd video.

---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour
+{
+	public int XPValue = 1;
+	public float dropPercentage = 0.5f;
+
+	public virtual void Die()
+	{
+		PlayerLevel.AddXP(XPValue);
+		if(Random.value <= dropPercentage)
+		{
+			WeaponGenerator.Instance.GenerateWeapon();
+		}
+	}
+}
---
+++ Assets/Scripts/OctoPushyDeath.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OctoPushyDeath : Death
+{
+	public GameObject particleEffect;
+	public string animationTrigger = "Death";
+
+	private Animator animator;
+
+	void Start()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	public override void Die()
+	{
+		base.Die();
+		animator.SetTrigger(animationTrigger);
+		Instantiate(particleEffect,transform.position,transform.rotation);
+
+		StartCoroutine(CheckAnimationDone());
+	}
+
+	IEnumerator CheckAnimationDone()
+	{
+		yield return null;
+		while(animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 1.0f && !animator.IsInTransition(0))
+		{
+			yield return null;
+		}
+
+		Destroy(gameObject);
+	}
+}
