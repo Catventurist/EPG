c9d6635f98321e42ee381b9da6c1f5f037c31b5a
Micefy <miccheli@gmail.com>
2015-05-07 12:21:25 +0300

    Scripts from 10th vid.

---
+++ Assets/Scripts/DamageInfo.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageInfo : MonoBehaviour
+{
+	public float minDamage;
+	public float maxDamage;
+
+	public float RollDamage()
+	{
+		return Random.Range(minDamage,maxDamage);
+	}
+}
